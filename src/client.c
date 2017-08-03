da5249c20501d7d2e7c84c59103b22b14df65fa5
Drew Bell <drew@5minofme.net>
2017-08-03 21:48:17 +0900

    updateing client

--- src/client.c
+++ src/client.c
@@ -1,6 +1,16 @@
 #include "client.h"
+typedef struct client
+{
+	/* The bufferedevent for this client. */
+	struct event_base *base;
+	struct bufferevent *client_buf_ev;
 
-#define EVLOOP_NO_EXIT_ON_EMPTY 0x04
+	evutil_socket_t client_fd;
+
+	/* The clients socket. */
+	struct timeval *event_timer;
+	int error_count;
+} Client;
 
 /**
  * Called on when there is data to read on the socket
@@ -10,6 +20,7 @@
  */
 void read_callback(struct bufferevent *bev, void *ctx)
 {
+	Client *client = (Client *)ctx;
 	int n;
 	char buf[BUF_SIZE];
 	char buffer[BUF_SIZE];
@@ -46,6 +57,7 @@ void read_callback(struct bufferevent *bev, void *ctx)
  */
 void event_callback(struct bufferevent *bev, short events, void *ctx)
 {
+	Client *client = (Client *)ctx;
 	if (events & BEV_EVENT_CONNECTED) {
 
 		LOG_WRITE("Connected\n");
@@ -53,30 +65,95 @@ void event_callback(struct bufferevent *bev, short events, void *ctx)
 
 		LOG_WRITE("Connection closed.\n");
 
-		sleep(5);
+// if there is no server address,
+#ifdef HAVE_AVAHI
+		if (conf->avahi || NULL == conf->server_address) {
+			do
+			{
+				avahi_client();
+			} while (NULL == conf->server_address);
+		}
+#endif
 
-		/* cleanup */
-		bufferevent_free(bev);
+		if (NULL == conf->server_address) {
+			LOG_WRITE("No server address\n");
+			if (conf->log_fd) {
+				fclose(conf->log_fd);
+			}
+			exit(EXIT_FAILURE);
+		}
 
-		/* Exit the current loop */
-		event_base_loopexit(ctx, NULL);
+		/* Get and connect a socket */
+		do
+		{ if (client->client_fd) close(client->client_fd);
+		  client->client_fd = get_socket();
+		} while (client->client_fd <= 0);
+
+		LOG_WRITE("New network socket is %i\n", client->client_fd);
+		bufferevent_setfd(client->client_buf_ev, client->client_fd);
+		bufferevent_enable(client->client_buf_ev, EV_READ);
+
+		++client->error_count;
+
+		// recurring error forces restart
+		if (client->error_count > 5) {
+		LOG_WRITE("Error count > 5 : restarting\n");
+
+			sleep(5);
+
+			/* Exit the current loop */
+			event_base_loopbreak(client->base);
 
 		/* Start again */
-		client_run_loop();
+			control_run_loop();
+		}
+
 	}else if (events & BEV_EVENT_ERROR)  {
 
 		LOG_WRITE("Got an error on the connection :%s\n", strerror(errno));
 
-		sleep(5);
+// if there is no server address,
+#ifdef HAVE_AVAHI
+		if (conf->avahi || NULL == conf->server_address) {
+			do
+			{
+				avahi_client();
+			} while (NULL == conf->server_address);
+		}
+#endif
 
-		/* cleanup */
-		bufferevent_free(bev);
+		if (NULL == conf->server_address) {
+			LOG_WRITE("No server address\n");
+			if (conf->log_fd) {
+				fclose(conf->log_fd);
+			}
+			exit(EXIT_FAILURE);
+		}
 
-		/* Exit the current loop */
-		event_base_loopexit(ctx, NULL);
+		/* Get and connect a socket */
+		do
+		{ if (client->client_fd) close(client->client_fd);
+		  client->client_fd = get_socket();
+		} while (client->client_fd <= 0);
+
+		LOG_WRITE("New network socket is %i\n", client->client_fd);
+		bufferevent_setfd(client->client_buf_ev, client->client_fd);
+		bufferevent_enable(client->client_buf_ev, EV_READ);
+
+		++client->error_count;
+
+		// recurring error forces restart
+		if (client->error_count > 5) {
+		LOG_WRITE("Error count > 5 : restarting\n");
+
+			sleep(5);
+
+			/* Exit the current loop */
+			event_base_loopbreak(client->base);
 
 		/* Start again */
-		client_run_loop();
+			control_run_loop();
+		}
 	}
 }
 
@@ -89,6 +166,13 @@ int client_run_loop()
 
 	LOG_WRITE("Starting Client\n");
 	log_config(conf);
+
+	Client *client = malloc(sizeof(Client));
+	client->client_fd = 0;
+	client->client_buf_ev = NULL;
+	client->error_count = 0;
+	client->event_timer = NULL;
+	client->base = NULL;
 
 	/* Kill any running video players */
 	if (!stop_video()) {
@@ -119,8 +203,9 @@ int client_run_loop()
 
 	/* libevent base object */
 	base = event_base_new();
-
-	if (!base) {
+	client->base = base;
+	if (!base)
+	{
 		LOG_WRITE("Could not initialize libevent!: %s\n", strerror(errno));
 		if (conf->log_fd) {
 			fclose(conf->log_fd);
@@ -129,13 +214,14 @@ int client_run_loop()
 	}
 
 	/* socket file descriptor */
-	int listen_fd = 0;
+	evutil_socket_t listen_fd = 0;
 
 	/* Get  and connect a socket */
 	do
 	{
 		listen_fd = get_socket();
 	} while (listen_fd <= 0);
+	client->clent_fd = listen_fd;
 
 	LOG_WRITE("Connected\n");
 
@@ -143,11 +229,13 @@ int client_run_loop()
 	bev = bufferevent_socket_new(base, listen_fd, BEV_OPT_CLOSE_ON_FREE);
 	if (!bev)
 		exit(EXIT_FAILURE);
+	client->client_buf_ev = bev;
+
 	/* set the callbacks */
-	bufferevent_setcb(bev, read_callback, NULL, event_callback, base);
+	bufferevent_setcb(bev, read_callback, NULL, event_callback, client);
 
 	/* enable reading */
-	bufferevent_enable(bev, EV_READ | EV_PERSIST);
+	bufferevent_enable(bev, EV_READ);
 
 	/* Start the event loop */
 	event_base_loop(base, EVLOOP_NO_EXIT_ON_EMPTY);
